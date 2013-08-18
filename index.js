75dcd8d0067079e2c81c763f7e72f865a3544822
Leonhardt Wille <hallo@leonhardt-wille.de>
2013-08-18 20:05:10 +0200

    improved optimist config

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var optimist = require('optimist'),
- argv = optimist
- .usage('Usage: $0 --aff [affixFile.aff]')
- .demand(['a'])
- .alias('a', 'aff')
- .describe('a', '.aff (Hunspell affix) file to load')
- .argv,
+var optimist = require('optimist')
+ .usage('Usage: $0 --lang [en_US]')
+ .demand(['l'])
+ .alias('l', 'lang')
+ .describe('l', 'Language to load')
+ .default('l', 'en_US')
+ .describe('help', 'Show this help'),
+ argv = optimist.argv,
  AffixParser = require('./src/hunspell/affix').AffixParser;
 
-affixParser = new AffixParser({path: argv.aff});
+if (argv.help) {
+  optimist.showHelp();
+  process.exit(0);
+}
+
+affixParser = new AffixParser({path: argv.lang + '.aff'});
 
 affixParser.parse(function(err){
   if (err) return console.error(err);
